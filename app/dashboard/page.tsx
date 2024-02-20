40f62e7aef2fde3a90e170e758654fd3164c85bc
CreativeDad <oyebolang@gmail.com>
2024-02-20 14:28:07 +0100

    add .

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return( <p className="text-blue-500 text-center py-10 font-bold text-4xl">Dashboard</p> )
+}
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -27,7 +27,7 @@ export default async function CardWrapper() {
         type="customers"
       /> */}
     </>
-  );
+  )
 }
 
 export function Card({
